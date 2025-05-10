de3426c3675cf20181f78d2386c68e4b6125463a
Kartikesh <44187008+kartikesh5232@users.noreply.github.com>
2025-05-10 10:23:38 +0530

    first

---
+++ src/main/java/com/example/gatewayService/GatewayServiceApplication.java
@@ -0,0 +1,21 @@
+package com.example.gatewayService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+}
