8739730ce076401ff259b8bf4b2caa1d39b009bc
Kartikesh <44187008+kartikesh5232@users.noreply.github.com>
2025-05-14 17:53:20 +0530

    Update GatewayServiceApplication.java

--- src/main/java/com/example/gatewayService/GatewayServiceApplication.java
+++ src/main/java/com/example/gatewayService/GatewayServiceApplication.java
@@ -15,7 +15,6 @@ import java.util.List;
 @EnableDiscoveryClient
 public class GatewayServiceApplication {
 
-	
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
